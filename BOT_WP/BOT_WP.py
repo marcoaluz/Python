367fe2588bffa47378c89d196b3f8fbb9c287289
Marco Aurelio <marcoa.luz@hotmail.com>
2023-09-21 21:56:43 -0300

    Final do Bot

--- BOT_WP/BOT_WP.py
+++ BOT_WP/BOT_WP.py
@@ -1,9 +1,82 @@
-#Bot de processos #
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import os
 import time
+import requests
 
-driver = webdriver.Chrome()
+
+#API 
+agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+
+#CHAVE    Zn0eZTEmtRg6YAI3XDFYLwmax8GizTFX
+api = requests.get("https://editacodigo.com.br/index/api-whatsapp/Zn0eZTEmtRg6YAI3XDFYLwmax8GizTFX" ,  headers=agent)
+time.sleep(1)
+api = api.text
+api = api.split(".n.")
+bolinha_notificacao = api[3].strip()
+contato_cliente = api[4].strip()
+caixa_msg = api[5].strip()
+msg_cliente = api[6].strip()
+caixa_msg2 = api[7].strip()
+caixa_pesquisa = api[8].strip()
+
+
+
+
+dir_path = os.getcwd()
+chrome_options2 = Options()
+chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/pasta/sessao")
+driver = webdriver.Chrome(options=chrome_options2)
 driver.get('https://web.whatsapp.com/')
+time.sleep(10)
 
-time.sleep(120)
+def bot():
+    try:
+        
+        #Click no botão verde
+        
+        bolinha = driver.find_element(By.CLASS_NAME,bolinha_notificacao)
+        bolinha = driver.find_elements(By.CLASS_NAME,bolinha_notificacao)
+        clica_bolinha = bolinha[-1]
+        acao_bolinha = webdriver.common.action_chains.ActionChains(driver)
+        acao_bolinha.move_to_element_with_offset(clica_bolinha,0,-20)
+        acao_bolinha.click()
+        acao_bolinha.perform()
+        acao_bolinha.click()
+        acao_bolinha.perform()
+        
+        
+        #Pegar o contato do cliente
+        telefone_cliente = driver.find_element(By.XPATH,contato_cliente)
+        telefone_final = telefone_cliente.text
+        print(telefone_final)
+        time.sleep(2)
+        
+        #Pegar msg do cliente
+        
+        todas_as_msg = driver.find_element(By.CLASS_NAME,msg_cliente)
+        todas_as_msg_texto = [e.text for e in todas_as_msg]
+        msg = todas_as_msg_texto[-1]
+        print(msg)
+        time.sleep(10)        
+        
+        #Respondendo Cliente
+        campo_de_texto = driver.find_element(By.XPATH,caixa_msg)
+        campo_de_texto.click()
+        time.sleep(1)
+        campo_de_texto.send_keys('Olá aqui é seu assistente virtual',Keys.ENTER)
+        
+        #Fechar Contato
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+        
+    except:
+        print('Aguardando novas msg')
+        
+        #entao vou tentar isso aqui 
+
+
+
+while True:
+    bot()
