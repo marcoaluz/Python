f38af0abd1766696e009e38ae997f8729e0493f2
Marco Aurelio <marcoa.luz@hotmail.com>
2023-09-26 22:08:23 -0300

    Aula

--- Pos_AULAS/Fundamentos de Machine Learning com Python/Visualização de dados com Seaborn.py
+++ Pos_AULAS/Fundamentos de Machine Learning com Python/Visualização de dados com Seaborn.py
@@ -1,8 +1,38 @@
 import pandas as pd
 
 
-dados_df = pd.read_csv('C:/Python/Pos_AULAS/Fundamentos de Machine Learning com Python/bank-full-aula.csv', delimiter=';')
+dados_df = pd.read_csv('C:/Python/Pos_AULAS/Fundamentos de Machine Learning com Python/bank-full-aula.csv', delimiter = ';')
 
 print(dados_df)
 
 
+print(list(dados_df.columns))
+print(dados_df.dtypes)
+
+#Primeiro elemento da base : .head()
+
+print(dados_df.head(3))
+
+
+#elementos finais da base: .TAIL()
+print(dados_df.tail())
+
+#Amostra aleatória da base: sample()
+
+print(dados_df.sample(50))
+
+#Analise exploratoria inicial dos dados
+
+print(dados_df.describe())
+
+# Explorando os campos com dados categoricos(object)
+
+
+
+print(dados_df['education'].unique())
+
+#Aoenas os valores nulos de education
+
+unknown_count = dados_df[dados_df['education'] == 'unknown']
+
+print(unknown_count)
